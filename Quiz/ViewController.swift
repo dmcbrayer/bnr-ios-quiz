0f59e41d44858c6508ff12d7e939088ea1d13d73
dmcbrayer <dmcbrayer@gmail.com>
2016-05-04 16:43:02 -0400

    Adds code to replace button text and stuff.

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,7 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
-
+    
+    let questions: [String] = ["What is your name?",
+                               "What is your quest?",
+                               "What is your favorite color?"]
+    
+    let answers: [String] = ["King Arthur",
+                             "To find the holy grail",
+                             "I don't know that"]
+    
+    var currentQuestionIndex: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+ 
 }
